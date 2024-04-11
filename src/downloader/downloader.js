c816e31a1fccb1943006a5cf785d6e97121abed2
ndriodna <kurodesu6969@gmail.com>
2024-04-12 00:22:41 +0800

    add: mkdir lesson and batch file

--- src/downloader/downloader.js
+++ src/downloader/downloader.js
@@ -1,11 +1,23 @@
 import { exec } from "node:child_process";
 import { readFileSync } from "node:fs";
+import { mkdir, writeFile } from "node:fs/promises";
+import { checkExist } from "../helper/helper.js";
 
-export default function (selected, courses) {
+export default async function (lesson) {
   const outputPath = "./output";
-  const filePath = "./batch.txt";
-  if (existsSync(filePath)) {
-    const getBatch = readFileSync(filePath);
-    exec(`'"./yt-dlp.exe", --refer https://vueschool.io/ -a ${getBatch}'`);
+  const batchPath = "batch.txt";
+  for (let i = 0; i < lesson.length; i++) {
+    const slugTitle = lesson[i].title.replace(/[ *]/g, "-");
+    await mkdir(`${outputPath}/${slugTitle}`, { recursive: true });
+    if (checkExist(`${outputPath}/${slugTitle}`)) {
+      await writeFile(
+        `${outputPath}/${slugTitle}/${batchPath}`,
+        String(lesson[i].urls)
+      );
+    }
   }
+  // if (existsSync(batchPath)) {
+  //   const getBatch = readFileSync(batchPath);
+  //   exec(`'"./yt-dlp.exe", --refer https://vueschool.io/ -a ${getBatch}'`);
+  // }
 }
