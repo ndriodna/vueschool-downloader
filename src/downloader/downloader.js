546597cbffd7a351186bbe0f5d923742b2f27eb4
ndriodna <kurodesu6969@gmail.com>
2024-04-12 23:06:37 +0800

    fix: download path, yt-dlp path

--- src/downloader/downloader.js
+++ src/downloader/downloader.js
@@ -1,23 +1,39 @@
-import { exec } from "node:child_process";
-import { readFileSync } from "node:fs";
+import { execFile } from "node:child_process";
 import { mkdir, writeFile } from "node:fs/promises";
 import { checkExist } from "../helper/helper.js";
 
 export default async function (lesson) {
   const outputPath = "./output";
   const batchPath = "batch.txt";
+  const listPath = [];
   for (let i = 0; i < lesson.length; i++) {
     const slugTitle = lesson[i].title.replace(/[ *]/g, "-");
-    await mkdir(`${outputPath}/${slugTitle}`, { recursive: true });
-    if (checkExist(`${outputPath}/${slugTitle}`)) {
+    const pathFile = `${outputPath}/${slugTitle}`;
+    await mkdir(pathFile, { recursive: true });
+    if (checkExist(pathFile)) {
       await writeFile(
-        `${outputPath}/${slugTitle}/${batchPath}`,
-        String(lesson[i].urls)
+        `${pathFile}/${batchPath}`,
+        String(lesson[i].urls).replace(/[,]/g, "\n")
+      );
+      listPath.push(pathFile);
+    }
+  }
+  for (let i = 0; i < listPath.length; i++) {
+    if (checkExist(`${listPath[i]}/${batchPath}`)) {
+      execFile(
+        "yt-dlp.exe",
+        [
+          "--refer https://vueschool.io/",
+          `-a ${listPath[i]}/${batchPath}`,
+          `-P ${listPath[i]}`,
+        ],
+        (error, stdout) => {
+          if (error) {
+            throw error;
+          }
+          console.log(stdout);
+        }
       );
     }
   }
-  // if (existsSync(batchPath)) {
-  //   const getBatch = readFileSync(batchPath);
-  //   exec(`'"./yt-dlp.exe", --refer https://vueschool.io/ -a ${getBatch}'`);
-  // }
 }
