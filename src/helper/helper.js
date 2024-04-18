027215c535e78b6bdf690b2bac2d78e8402fb864
ndriodna <kurodesu6969@gmail.com>
2024-04-18 15:36:38 +0800

    change: check to colorText

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -8,12 +8,38 @@ function starLine() {
   console.log();
 }
 
-function check() {
+function colorText(color, text) {
   const colors = {
     reset: "\x1b[0m",
-    green: "\x1b[32m \u2713 ",
+    greenCheck: "\x1b[32m \u2713 ",
+    red: "\x1b[31m",
+    green: "\x1b[32m",
+    yellow: "\x1b[33m",
+    blue: "\x1b[34m",
+    magenta: "\x1b[35m",
+    cyan: "\x1b[36m",
+    white: "\x1b[37m",
   };
-  return colors.green + colors.reset;
+  switch (color) {
+    case 0:
+      return colors.greenCheck + colors.reset;
+    case 1:
+      return colors.red + text + colors.reset;
+    case 2:
+      return colors.green + text + colors.reset;
+    case 3:
+      return colors.yellow + text + colors.reset;
+    case 4:
+      return colors.blue + text + colors.reset;
+    case 5:
+      return colors.magenta + text + colors.reset;
+    case 6:
+      return colors.cyan + text + colors.reset;
+    case 7:
+      return colors.white + text + colors.reset;
+    default:
+      return colors.white + text + colors.reset;
+  }
 }
 async function checkExist(path) {
   try {
@@ -29,4 +55,4 @@ function clearScreen(readline) {
   readline.clearScreenDown(process.stdout);
 }
 
-export { starLine, check, checkExist, clearScreen };
+export { starLine, colorText, checkExist, clearScreen };
